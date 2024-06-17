fe6be510a8402f0d0e6338ed6a7c7e149ee28396
Brendan Dempsey <bjd1412@gmail.com>
2024-06-17 03:32:37 -0400

    added a lot

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 # lib/debug.py
 
-from models.__init__ import CONN, CURSOR
+from __init__ import CONN, CURSOR
+from publisher import Publisher
 import ipdb
 
+def reset_database():
+    Publisher.drop_table()
+    Publisher.create_table()
 
+
+
+
+
+reset_database()
 ipdb.set_trace()
---
+++ lib/publisher.py
@@ -0,0 +1,52 @@
+#lib/publisher.py
+from __init__ import CURSOR, CONN
+
+
+
+class Publisher:
+    all = {}
+
+    def __init__(self, company_name, location, id=None):
+        self.id = id
+        self.company_name = company_name
+        self.location = location
+
+    def __repr__(self):
+        return f"<Publisher {self.id}: {self.company_name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS publishers (
+            id INTEGER PRIMARY KEY,
+            company_name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS publishers;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO publishers(company_name, location) VALUES(?, ?)
+        """
+        CURSOR.execute(sql, (self.company_name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls, company_name, location):
+        publisher = cls(company_name, location)
+        publisher.save()
+        return publisher
+ 
+    def update(self):
+        pass
