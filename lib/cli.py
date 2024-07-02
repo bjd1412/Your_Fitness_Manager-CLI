a9937a005ec488c2592bc6bfce6f38acaafe4d20
Brendan Dempsey <bjd1412@gmail.com>
2024-07-02 00:18:05 -0400

    Working

--- lib/cli.py
+++ lib/cli.py
@@ -5,32 +5,57 @@ from models.exercise import Exercise
 from helpers import (
     all_fitness,
     exit_program,
-    fitness_exercises
+    fitness_exercises,
+    add_fitness
 )
 
 
 def home():
+    print("********************Welcome To Your Fitness Manager********************")
+    print()
+    print()
+    print("Press 1 to see all of your training types.")
+    print("Press 0 to exit Your Fitness Manager.")
     while True:
         print()
-        print()
-        print("********************Welcome To Your Fitness Manager********************")
-        print()
-        print()
-        print()
-        all_fitness()
-        print()
-        print()
-        print()
-        print("Press 1 to see all exercises in your training type.")
-        print("Press 0 to exit")
         choice = input(">")
         if choice == "1":
-            fitness_exercises()
-            pass
+            home2()
         elif choice == "0":
             exit_program()
         else:
             print("Invalid Choice")
+
+def home2():
+    print()
+    print()
+    print("***************")
+    print()
+    all_fitness()
+    print()
+    print("***************")
+    print()
+    while True:
+        print()
+        print()
+        print("Press 1 to see all exercises for your training type.")
+        print("Press 2 to add a training type.")
+        print("Press 0 to exit Your Fitness Manager.")
+    
+        choice = input("> ")
+        if choice == "1":
+            fitness_exercises()
+        elif choice == "2":
+            add_fitness()
+        elif choice == "0":
+            exit_program()
+        else:
+            print("Invalid Choice!") 
+
+def home3():
+    pass
+
+
 
 
 
--- lib/helpers.py
+++ lib/helpers.py
@@ -14,18 +14,47 @@ def all_fitness():
     for i, fit in enumerate(fitness, start = 1):
         print(f"{i}: {fit.training} | {fit.target}")
 
-def fitness_exercises():
-    idd = input()
-    exercise = Fitness.exercises(idd)
-    for i, ex in enumerate(exercise, start = 1):
-        return exercise[int(i) - 1]
-        print(f"{i}: {ex.name} | {ex.reps}")
+# def fitness_exercises():
+#     id_ = input("Enter training id: ")
+#     fitness = Fitness.find_by_id(id_)
+#     if fitness:
+#         exercise = fitness.exercises()
+#         for i, ex in enumerate(exercise, start = 1):
+#             print(f"{i}: {ex.name} | {ex.reps}")
+#     else:
+#         print("Nothing")   
+         
     
 
 def all_exercises():
     exercises = Exercise.get_all()
     for i, ex in enumerate(exercises, start =1):
         print(f"{i}: {ex.name} | {ex.repsh}")
+
+def fitness_exercises():
+    name = input("Please enter the training type: ")
+    fitness = Fitness.find_by_name(name)
+    if fitness:
+        exercise = fitness.exercises()
+        for i, ex in enumerate(exercise, start = 1):
+            print(f"{i}: {ex.name} | {ex.reps}")
+    else:
+        print("Training type was either entered innocorrectly, or does not exist.")
+
+def add_fitness():
+    training = input("Please enter the name of your training type: ")
+    target = input("Please enter the training types target target focus: ")
+    try:
+        Fitness.create(training, target)
+        print("Fitness type succesfully created!")
+    except:
+        print("Failed to create fitness type. Please ensure you have entered in all details correctly.")
+
+def add_exercise():
+    pass
+
+    
+    
 
 
 
