9feb98e1ff1f80e6535f02aedc144167b5b5a420
Brendan Dempsey <bjd1412@gmail.com>
2024-07-05 23:34:19 -0400

    up to date

--- lib/cli.py
+++ lib/cli.py
@@ -21,7 +21,7 @@ def home():
     print("Press 0 to exit Your Fitness Manager.")
     while True:
         print()
-        choice = input(">")
+        choice = input("> ")
         if choice == "1":
             home2()
         elif choice == "0":
@@ -44,15 +44,15 @@ def home2():
         print("Press 1 to see all exercises for your training type.")
         print("Press 2 to add a training type.")
         print("Press 3 to delete your training type.")
-        print("Press 0 to exit Your Fitness Manager.")
-    
+        print("Press 0 to exit Your Fitness Manager.")    
         choice = input("> ")
         if choice == "1":
             name = input("Please enter the training type: ")
             fitness = Fitness.find_by_name(name)
             if fitness:
-                print(f"      {fitness.training}")
-                print("***************")
+                print()
+                print(f"{fitness.training}")
+                print("*******************")
                 exercise = fitness.exercises()
                 for i, ex in enumerate(exercise, start = 1):
                     print(f"{i}: {ex.name} | {ex.reps}")
@@ -87,17 +87,6 @@ def home3(fitness):
             exit_program()
         else:
             print("Invalid choice.")
-
-
-
-
-
-
-
-
-
-
-
 
 
 
